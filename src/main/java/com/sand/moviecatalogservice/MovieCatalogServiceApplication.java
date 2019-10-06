a651cfe95db01c6e2d45668f04a5145614700099
Sand038 <sandeepamadhusanka038@gmail.com>
2019-10-06 22:14:46 +0530

    Adding changes after Eureka configured

--- src/main/java/com/sand/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/sand/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,13 +2,18 @@ package com.sand.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication @EnableEurekaClient public class MovieCatalogServiceApplication
+@SpringBootApplication
+@EnableEurekaClient
+public class MovieCatalogServiceApplication
 {
-  @Bean public RestTemplate getRestTemplate()
+  @Bean
+  @LoadBalanced
+  public RestTemplate getRestTemplate()
   {
     return new RestTemplate();
   }
--- src/main/java/com/sand/moviecatalogservice/resource/MovieCatalogResource.java
+++ src/main/java/com/sand/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,30 +1,34 @@
 package com.sand.moviecatalogservice.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.sand.moviecatalogservice.model.CatalogItem;
 import com.sand.moviecatalogservice.model.Movie;
-import com.sand.moviecatalogservice.model.Rating;
 import com.sand.moviecatalogservice.model.UserRating;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@RestController @RequestMapping("/catalog") public class MovieCatalogResource
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource
 {
-  @Autowired private RestTemplate restTemplate;
+  @Autowired
+  private RestTemplate restTemplate;
 
-  @RequestMapping("/{userId}") public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
+  @RequestMapping("/{userId}")
+  public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
   {
-    UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+    UserRating userRating = restTemplate
+        .getForObject("http://movie-rating-service/ratingsdata/users/" + userId, UserRating.class);
 
     return userRating.getRatings().stream().map(rating -> {
-      Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-      return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+      Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+      return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
     }).collect(Collectors.toList());
   }
 }
