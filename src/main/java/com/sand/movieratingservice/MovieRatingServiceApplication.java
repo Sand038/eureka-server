22ad642c36c18c75947a3ee05f07379edbb9e528
Sand038 <sandeepamadhusanka038@gmail.com>
2019-10-06 22:38:57 +0530

    Adding changes after Eureka configured

--- src/main/java/com/sand/movieratingservice/MovieRatingServiceApplication.java
+++ src/main/java/com/sand/movieratingservice/MovieRatingServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication @EnableEurekaClient public class MovieRatingServiceApplication
+@SpringBootApplication
+@EnableEurekaClient
+public class MovieRatingServiceApplication
 {
 
   public static void main(String[] args)
