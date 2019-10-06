8ae47aeeba98fd73b3a77123d8d23a9d7d989726
Sand038 <sandeepamadhusanka038@gmail.com>
2019-10-06 22:15:43 +0530

    Adding changes after Eureka configured

--- src/main/java/com/sand/movieinfoservice/model/Movie.java
+++ src/main/java/com/sand/movieinfoservice/model/Movie.java
@@ -6,14 +6,17 @@ public class Movie
 
   private String name;
 
+  private String description;
+
   public Movie()
   {
   }
 
-  public Movie(String movieId, String name)
+  public Movie(String movieId, String name, String description)
   {
     this.movieId = movieId;
     this.name = name;
+    this.description = description;
   }
 
   public String getMovieId()
@@ -34,5 +37,15 @@ public class Movie
   public void setName(String name)
   {
     this.name = name;
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
   }
 }
--- src/main/java/com/sand/movieinfoservice/resource/MovieResource.java
+++ src/main/java/com/sand/movieinfoservice/resource/MovieResource.java
@@ -1,14 +1,29 @@
 package com.sand.movieinfoservice.resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.sand.movieinfoservice.model.Movie;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController @RequestMapping("/movies") public class MovieResource
+@RestController
+@RequestMapping("/movies")
+public class MovieResource
 {
-  @RequestMapping("/{movieId}") public Movie getMovieInfo(@PathVariable("movieId") String movieId)
+  @RequestMapping("/{movieId}")
+  public Movie getMovieInfo(@PathVariable("movieId") String movieId)
   {
-    return new Movie(movieId, "Test name");
+    List<Movie> movies = Arrays.asList(
+        new Movie("123", "Joker",
+        "DescriptionForever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City"),
+        new Movie("234", "Hobbs and Shaw",
+        "DescriptionBrixton Lorr is a cybernetically enhanced soldier who possesses superhuman strength, a brilliant mind and a lethal pathogen that could wipe out half of the world's population" ),
+        new Movie("345", "Captain Marvel",
+        "DescriptionCaptain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls")
+    );
+    return movies.stream().filter(m -> m.getMovieId().equals(movieId)).findFirst().get();
   }
 }
