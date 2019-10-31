25f109451c36eac748e0b263ee3d089334d227c8
Sand038 <sandeepamadhusanka038@gmail.com>
2019-10-31 21:15:14 +0530

    Adding changes for calling for external movieDB api

--- src/main/java/com/sand/movieinfoservice/resource/MovieResource.java
+++ src/main/java/com/sand/movieinfoservice/resource/MovieResource.java
@@ -1,29 +1,30 @@
 package com.sand.movieinfoservice.resource;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.sand.movieinfoservice.model.Movie;
+import com.sand.movieinfoservice.model.MovieSummary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource
 {
+  @Value("${api.key}")
+  private String apiKey;
+
+  @Autowired
+  private RestTemplate restTemplate;
+
   @RequestMapping("/{movieId}")
   public Movie getMovieInfo(@PathVariable("movieId") String movieId)
   {
-    List<Movie> movies = Arrays.asList(
-        new Movie("123", "Joker",
-        "DescriptionForever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City"),
-        new Movie("234", "Hobbs and Shaw",
-        "DescriptionBrixton Lorr is a cybernetically enhanced soldier who possesses superhuman strength, a brilliant mind and a lethal pathogen that could wipe out half of the world's population" ),
-        new Movie("345", "Captain Marvel",
-        "DescriptionCaptain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls")
-    );
-    return movies.stream().filter(m -> m.getMovieId().equals(movieId)).findFirst().get();
+    MovieSummary movieSummary = restTemplate
+        .getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+    return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
   }
 }
